9d4f1689ebd4da19d0a0595d00541d9ed8319d0d
KevinBel <kevin.beltran@estudiantesunibague.edu.co>
2025-10-20 21:01:56 -0500

    Solucion error al correr en android

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.JavaVersion
+
 allprojects {
     repositories {
         google()
@@ -17,6 +20,24 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    if (name == "isar_flutter_libs") {
+        plugins.withId("com.android.library") {
+            extensions.configure<LibraryExtension> {
+                namespace = "dev.isar.isar_flutter_libs"
+                compileSdk = 34
+                defaultConfig {
+                    minSdk = 21
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_11
+                    targetCompatibility = JavaVersion.VERSION_11
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
